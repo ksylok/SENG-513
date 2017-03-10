e03118e94cc602ace92ae865d0e964e7a7bbf38a
Kristie Lok <kristie.lok@hotmail.com>
2017-03-10 04:51:28 -0700

    Bottom-up scroll and text is done. Edited app colours.

--- Assignment3/public/client.js
+++ Assignment3/public/client.js
@@ -4,30 +4,29 @@ $(function() {
     var nickname;
     
     $('form').submit(function(){
-	   socket.emit('chat', { message: $('#m').val()});
-        socket.emit('notice', { message: $('#m').val()});
-        socket.emit('nickname', { message: $('#m').val()});
+        socket.emit('chat', { message: $('#m').val()});
 	   $('#m').val('');
 	   return false;
     });
 
     socket.on('chat', function(msg){
         var hexcolor = msg.userInfo.nameColour;
+        
         // messages sent by user is bolded, otherwise regular style
         if(msg.userInfo.clientKey === socket.io.engine.id){
-            $('#messages').append($('<li>').html(msg.time + "  ||  <b><font color=\"" + hexcolor + "\">" + msg.userInfo.nickname + "</font>: " + msg.message + "</b>"));
+            $('#messages').prepend($('<li>').html(msg.time + "  ||  <b><font color=\"" + hexcolor + "\">" + msg.userInfo.nickname + "</font>: " + msg.message + "</b>"));
         }
         else{
-            $('#messages').append($('<li>').html(msg.time + "  ||  <b><font color=\"" + hexcolor + "\">" + msg.userInfo.nickname + "</font></b>: " + msg.message));
+            $('#messages').prepend($('<li>').html(msg.time + "  ||  <b><font color=\"" + hexcolor + "\">" + msg.userInfo.nickname + "</font></b>: " + msg.message));
         }
     });
     
     socket.on('notice', function(msg){
-	   $('#messages').append($('<li>').html(msg.time + "  ||  " + '<i>' + msg.chatNotice + '</i>'));
+	   $('#messages').prepend($('<li>').html(msg.time + "  ||  " + '<i>' + msg.chatNotice + '</i>'));
     });
     
     socket.on('nickname', function(msg){
-	   $('#messages').append($('<li>').html('<b><i>' + msg.nickname + '</b></i>'));
+	   $('#messages').prepend($('<li>').html('<b><i>' + msg.nickname + '</b></i>'));
     });
     
     // clears user list, and prints new one
@@ -35,7 +34,7 @@ $(function() {
         $('#userlist').empty();
         for (let i = 0; i < msg.userList.length; i++){
             let hexcolor = msg.userList[i].nameColour;
-            $('#userlist').append($('<li>').html("<b><font color=\"" + hexcolor + "\">" + msg.userList[i].nickname + "</font>"));   
+            $('#userlist').prepend($('<li>').html("<b><font color=\"" + hexcolor + "\">" + msg.userList[i].nickname + "</font>"));   
         }
     });
 });
